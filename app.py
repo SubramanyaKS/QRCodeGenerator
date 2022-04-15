386d41f263ce28b2e19f2ab8aa662d1aa4c47d40
Subramanya K S <subramanyaks2205@gmail.com>
2022-04-15 19:33:29 +0530

    updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, send_file
 from flask import request,render_template
 import qrcode.image.svg as qr
 import qrcode
-
+from barcode import EAN13
+from barcode.writer import ImageWriter
 
 app = Flask(__name__)
 @app.route('/')
@@ -20,10 +21,24 @@ def qr():
         img.save('static\qrcode.jpg')
     return render_template("result.html",img_data=img)
 
+@app.route('/bar', methods=['GET','POST'])
+def bar():
+    if request.method == 'POST':
+        number=request.form['num']
+        my_code = EAN13(number, writer=ImageWriter())
+        my_code.save("static/new_code")
+    return render_template("bar.html",img_data=my_code)
+
 @app.route('/download')
 def downloadFile ():
     #For windows you need to use drive name [ex: F:/Example.pdf]
     path = "./static/qrcode.jpg"
+    return send_file(path, as_attachment=True)
+
+@app.route('/downloadBar')
+def downloadBar ():
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    path = "./static/new_code.png"
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
